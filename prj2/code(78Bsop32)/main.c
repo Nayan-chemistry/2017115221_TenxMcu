3ded0587713fbe89bad9abe5fce98d6875cff998
Nayan-chemistry <1210148168@qq.com>
2019-11-29 14:18:39 +0800

    prj2实现按键控制LED灯亮灭

--- prj2/code(78Bsop32)/main.c
+++ prj2/code(78Bsop32)/main.c
@@ -10,10 +10,19 @@ void main() {
   F_led1On();
   DelayMs(500);
 
+ //使能看门狗
+  F_turnOnWDT();
   while (1) {
    //喂狗
    F_clearWDT();
 
+   //获取按键标志
+   GetKeyValue();
+   if (D_keyValue1 == keyValue) {
+      F_led1Neg();
+    }
+    //初始化keyValue值
+    keyValue = D_keyNull; 
 
   }
 }
@@ -23,7 +32,6 @@ void DelayMs(uint16_t msCount) {
  uint16_t i,j;
   for ( i = 0; i < msCount; i++) {
           for ( j = 0; j < 1000; j++) {
-
             F_clearWDT();
     }
           }
