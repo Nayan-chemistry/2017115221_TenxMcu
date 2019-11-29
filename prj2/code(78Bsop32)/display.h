098fc55bf7ca6c72cdd8bf2b9b468d9082dc4352
Nayan-chemistry <1210148168@qq.com>
2019-11-29 13:31:53 +0800

    添加display.h定义LED

---
+++ prj2/code(78Bsop32)/display.h
@@ -0,0 +1,33 @@
+#ifndef __display_h
+#define __display_h
+// 分别定义P_led1、P_led1、P_led1为p11、p12、p13引脚
+#define P_led1 P1_1
+#define P_led2 P1_2
+#define P_led3 P1_3
+// 定义高低电平变量
+#define D_ledOn 1  //高电位亮
+#define D_ledOff 0 //低点位灭
+// Globle Var -----------------------------------------
+#ifdef __display_c
+
+#else
+
+#endif
+
+// 定义LED1亮灯、关灯、取反函数
+#define F_led1On()  P_led1 = D_ledOn
+#define F_led1Off() P_led1 = D_ledOff
+#define F_led1Neg() P_led1 = ~P_led1  
+
+// 定义LED2亮灯、关灯、取反函数
+#define F_led2On() P_led2 = D_ledOn
+#define F_led2Off() P_led2 = D_ledOff
+#define F_led2Neg() P_led2 = ~P_led2  
+
+// 定义LED3亮灯、关灯、取反函数
+#define F_led3On() P_led3 = D_ledOn
+#define F_led3Off() P_led3 = D_ledOff
+#define F_led3Neg() P_led3 = ~P_led3  
+// Function ------------------------------------
+
+#endif 
