5a68e06e0f17b585ee18a4b5d6c2760f55173319
Nayan-chemistry <1210148168@qq.com>
2019-11-29 23:50:33 +0800

    prj3--实现LED灯呼吸灯功能

--- prj3/code(78Bsop32)/init.c
+++ prj3/code(78Bsop32)/init.c
@@ -1,10 +1,11 @@
-#define __init_c
+﻿#define __init_c
 #include "includeAll.h"
+
 //=============================================================================
 void SysInit() {
-  F_clearWDT();  //启动后立即清除看门狗
+ F_clearWDT();  //启动后立即清除看门狗
   //时钟初始化
-  CLKCON = 0x03; // Clock div 1  7.3728Mhz
+ CLKCON = 0x03; // Clock div 1// Clock div 1
   STPPCK = 0;
   STPFCK = 0;
   _nop_();
@@ -23,25 +24,22 @@ void SysInit() {
   _nop_();
   _nop_();
   _nop_();
-
-  //将IO模式置为上拉输入
-  P1MODL = 0xa8;
+  
+  // IO 模式初始化
+ P1MODL = 0xa8;       //将模式置为上拉输入
 
   // IO 状态初始化
-  F_ledOff();      //LED灯初始化为低电平状态（不亮）
+  F_led1Off();      //LED灯初始化为低电平状态（不亮）
 
-  // T2 初始化  7.3728Mhz/2 = 3686400Hz
-  //  3686400Hz/3686.4 = 1000Hz = 1ms timer2 interrupt
-  TH2 = (65536 - 900) / 256;
+  TH2 = (65536 - 900) / 256; // 0.25ms==4khz timer2 interrupt
   RCP2H = TH2;
   TL2 = (65536 - 900) % 256;
   RCP2L = TL2;
-     
-   
-  EA = 1;   // 开总中断
-  ET2 = 1;  // timer2中断使能
-  TR2 = 1;  // timer2运行
-}
 
+  EA = 1;  // 开总中断
+  ET2 = 1; // timer2中断使能
+  TR2 = 1; // timer2运行
+  
+}
 //=============================================================================
-void VarsInit() {}
+void VarsInit() {} 
