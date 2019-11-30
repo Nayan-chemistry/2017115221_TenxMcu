2e95126ae370cb25004951c2d13618d39dfcca58
Nayan-chemistry <1210148168@qq.com>
2019-12-01 00:15:43 +0800

    prj4--单端口复用按键控制LED亮灭实验

--- prj4/code(78Bsop32)/main.c
+++ prj4/code(78Bsop32)/main.c
@@ -2,7 +2,7 @@
 #include "includeAll.h"
 //=============================================================================
 
-int model = 1;           //状态初始化为1
+int model = 1;
 
 void main(){
   SysInit();
@@ -26,43 +26,40 @@ void main(){
 //============================================
 void DisplayProcess() {}
 //============================================
-void TimeProcess() {}
+void TaskProcess() {}
 //============================================
-void TaskSetting(){	}
-//=========单端口控制LED亮灭===================
-void TaskProcess() {
+void TimeProcess() {
 
-	//设置为上拉模式，获取按键值
-	    P1MODL = 0xa8;		
-		GetKeyValue();
-        DelayMs(100);		 //稍微延时，获取键值
+static uint8_t timer5ms = 0;
+static uint16_t second = 0;
 
-	if (model== 1){          //状态标志为1时，设置为推挽输出	
-		//按下按键
-		if(D_keyValue1 == keyValue){
-			model_Switch();	 //状态标志取反
-		}
-		P1MODL = 0xaa;		 //模式为推挽输出，点亮LED
-		
-	}
-	  else {                  //未按下按键，为上拉模式
-		//按下按键
-		if(D_keyValue1 == keyValue){
-			model_Switch();	  //状态标志取反
-		
-     	}
-
-	} 
-
+  if (b1ms) {
+    // 1ms 执行一次
+    b1ms = 0;
+    timer5ms++;
+  }
+  if (timer5ms >= 5) {
+		P1MODL = 0xa8;            //将模式置为上拉输入
+    	GetKeyValue();            //获取键值
+  }
 }
 
-
-void DelayMs(int time){
-	uint16_t i,j;
-	for(i=0; i<time; i++){
-		for(j=0; j<1000; j++){
+//=============================================================================
+void TaskSetting() {
+	
+    if (model == 1){              //模式标志为1时，该模式为推挽输出
+		//LED状态转换
+		if(D_keyValue1 == keyValue){
+			model = ~model;		  //模式标志为取反
 		}
-	}
+		P1MODL = 0xaa;		       //将模式置为推挽输出，点亮LED
+		}
+	else {
+	       //模式标志未改变，该模式为上拉输入
+			if(D_keyValue1 == keyValue){
+				model = ~model;   //模式标志为取反
+			}
+		}
 }
 
       
