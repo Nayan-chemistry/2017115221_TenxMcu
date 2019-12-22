2ca4412abf93429c6de36f911d243d485eaa339b
Nayan-chemistry <1210148168@qq.com>
2019-12-22 10:10:03 +0800

    初始化函数

---
+++ prj5_hotkettle/code(68Bsop32)/init.c
@@ -0,0 +1,48 @@
+#define __init_c
+#include "includeAll.h"
+//=============================================================================
+void SysInit() {
+	F_clearWDT();
+  //时钟初始化
+  CLKCON = 0x03; // Clock div 1  7.3728Mhz
+  STPPCK = 0;
+  STPFCK = 0;
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  SELFCK = 1;
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  // IO 模式初始化
+	P1MODL = 0xaa;		//将模式置为推挽输出
+	//P1MODH = 0x2a;
+	P1MODH = 0xaa;
+	P3MODL = 0xaa;
+	P3MODH = 0x2a;		//将模式置为上拉输入
+	//P1_7 = 1;
+	//P1_2 = 1;
+	
+  // IO 状态初始化
+	//F_ledOff();		//初始化为低电位（不亮）
+	
+	//T2初始化
+	TH2 = (65536 - 7373) / 256;
+  RCP2H = TH2;
+  TL2 = (65536 - 7373) % 256;
+  RCP2L = TL2;
+  TR2 = 1;
+  ET2 = 1;
+  EA = 1; 
+}
+//=============================================================================
+void VarsInit() {}
