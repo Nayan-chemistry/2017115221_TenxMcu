098be9e51b8ffbf77f6980cb51306c5622f69853
Nayan-chemistry <1210148168@qq.com>
2019-12-22 10:09:40 +0800

    头文件定义

---
+++ prj5_hotkettle/code(68Bsop32)/includeAll.h
@@ -0,0 +1,11 @@
+#include <REGtenxTM52F5268B.h>
+
+#include "typeAlias.h"
+
+#include "display.h"
+#include "init.h"
+#include "keys.h"
+#include "isr.h"
+#include "main.h"
+#include "buzz.h"
+#include <INTRINS.H>
