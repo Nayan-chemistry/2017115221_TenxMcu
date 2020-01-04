06536199ea2d8158abfb8f1bc787ded2063765c7
Nayan-chemistry <1210148168@qq.com>
2020-01-05 00:20:55 +0800

    修改数码管显示

--- prj5_hotkettle/code(68Bsop32)/display.c
+++ prj5_hotkettle/code(68Bsop32)/display.c
@@ -1,136 +1,52 @@
 #define __display_c
 #include "includeAll.h"
 
-code const uint8_t displayFonts[19] = { 0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80,0x00};
+code const uint8_t displayFonts[19] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80,0x00,0xff};
 
-void DisplayTenNumber(uint8_t number){
-	uint16_t temp1,temp2,temp3,temp4;
-	uint8_t i = 0,l=1;
-	temp1 = P1MODH;
-	temp2 = P3MODL;
-	temp3 = P3MODH;
-	temp4 = P1MODL;
-	//1.7
-	P1MODH = 0xAA;
-	//3.2
-	P3MODL = 0xAA;
-	//3.4
-	P3MODH = 0x2A;
-	//1.2
-	P1MODL = 0xAA;
-	
-	P1_6 = 1;
-	P1_1 = 0;
-	
+void SMG_Select(uint8_t position, uint8_t number)
+{
+    //选择数码管的位置
+    //根据电路图，电平为低时该位被选中
+    if (position == 1)
+    {
+        SMG_COM1 = 0;
+        SMG_COM2 = 1;
+    }
+    else if (position == 2)
+    {
+        SMG_COM1 = 1;
+        SMG_COM2 = 0;
+    }
+
+    //数位赋值
+    SMG_A = 0x01 & (displayFonts[number] >> 0);
+    SMG_B = 0x01 & (displayFonts[number] >> 1);
+    SMG_C = 0x01 & (displayFonts[number] >> 2);
+    SMG_D = 0x01 & (displayFonts[number] >> 3);
+    SMG_E = 0x01 & (displayFonts[number] >> 4);
+    SMG_F = 0x01 & (displayFonts[number] >> 5);
+    SMG_G = 0x01 & (displayFonts[number] >> 6);
+    SMG_DG = 0x01 & (displayFonts[number] >> 7);
+}
+
+void SMG_Display(uint8_t number)
+{
+		uint8_t i,j;
 		
-	P1_4 = displayFonts[number]&l;
-	l=l*2;
-	P3_4 = displayFonts[number]&l;
-	l=l*2;
-	P3_5 = displayFonts[number]&l;
-	l=l*2;
-	P1_7 = displayFonts[number]&l;
-	l=l*2;
-	P3_6 = displayFonts[number]&l;
-	l=l*2;
-	P1_2 = displayFonts[number]&l;
-	l=l*2;
-	P3_2 = displayFonts[number]&l;
-	
-
-	
-	P1MODH = temp1;
-	P3MODL = temp2;
-	P3MODH = temp3;
-	P1MODL = temp4;
-
+		if(number>100)
+			return;
+		//个位
+		j = number % 10;
+		SMG_Select(2, j);
+		delayMs(10);
+		//十位
+		i = (number - j)/10;
+		SMG_Select(1, i);
+		delayMs(10);
 }
 
-void DisplayBitNumber(uint8_t number){
-	uint16_t temp1,temp2,temp3,temp4;
-	uint8_t  i=0,l=1;
-	temp1 = P1MODH;
-	temp2 = P3MODL;
-	temp3 = P3MODH;
-	temp4 = P1MODL;
-	//1.7
-	P1MODH = 0xAA;
-	//3.2
-	P3MODL = 0xAA;
-	//3.4
-	P3MODH = 0x2A;
-	//1.2
-	P1MODL = 0xAA;
-	
-	P1_6 = 0;
-	P1_1 = 1;
-	
-	P1_4 = displayFonts[number]&l;
-	l=l*2;
-	P3_4 = displayFonts[number]&l;
-	l=l*2;
-	P3_5 = displayFonts[number]&l;
-	l=l*2;
-	P1_7 = displayFonts[number]&l;
-	l=l*2;
-	P3_6 = displayFonts[number]&l;
-	l=l*2;
-	P1_2 = displayFonts[number]&l;
-	l=l*2;
-	P3_2 = displayFonts[number]&l;
-	
-	P1MODH = temp1;
-	P3MODL = temp2;
-	P3MODH = temp3;
-	P1MODL = temp4;
-
-
-}
-
-void DisplayNumber(uint8_t number)
+void Set_Pin_Sta()
 {
-	uint8_t i,j;
-	if(number>100)
-		return;
-
-	j = number % 10;
-	DisplayBitNumber(j);
-
-	i = (number - j)/10;
-	DisplayTenNumber(i);
-
-}
-
-void ClearNumber(uint8_t number)
-{
-	uint16_t temp1,temp2,temp3,temp4;
-	uint8_t  i=0,l=1;
-
-	//1.7
-	P1MODH = 0xAA;
-	//3.2
-	P3MODL = 0xAA;
-	//3.4
-	P3MODH = 0x2A;
-	//1.2
-	P1MODL = 0xAA;
-	
-	P1_6 = 0;
-	P1_1 = 1;
-	
-	P1_4 = displayFonts[number]&l;	//a
-	l=l*2;
-	P3_4 = displayFonts[number]&l;	//b
-	l=l*2;
-	P3_5 = displayFonts[number]&l;	//c
-	l=l*2;
-	P1_7 = displayFonts[number]&l;	//d
-	l=l*2;
-	P3_6 = displayFonts[number]&l;	//e
-	l=l*2;
-	P1_2 = displayFonts[number]&l;	//f
-	l=l*2;
-	P3_2 = displayFonts[number]&l;	//g
-	l=l*2;
-	P1_3 = displayFonts[number]&l;	//dg
+		P1_2 = 1;
+		P1_7 = 1;
 }
--- prj5_hotkettle/code(68Bsop32)/display.h
+++ prj5_hotkettle/code(68Bsop32)/display.h
@@ -4,23 +4,27 @@
 #define P_ledcom P1_0	
 #define P_led2 P1_2
 #define P_led1 P1_4
-#define P_buzz P1_5
-#define P_smgcom1 P1_1
-#define P_smgcom2 P1_6
-//#define P_D P1_7
+
+#define SMG_COM1 P1_1
+#define SMG_COM2 P1_6
+
+#define SMG_A P1_4
+#define SMG_B P3_4
+#define SMG_C P3_5
+#define SMG_D P1_7
+#define SMG_E P3_6
+#define SMG_F P1_2
+#define SMG_G P3_2
+#define SMG_DG P1_3
 
 // Const: exp: #define D_data 1 ----------------
 #define D_ledOn 1  //高电位亮
 #define D_ledOff 0 //低点位灭
 // Globle Var -----------------------------------------
 #ifdef __display_c
-bit bflag0;
-bit bflag;
-uint8_t num;
+
 #else
-extern bit bflag0;
-extern bit bflag;
-extern uint8_t num;
+
 #endif
 
 // Action Macro: exp: #define F_getData() ------
@@ -28,8 +32,7 @@ extern uint8_t num;
 #define F_ledOff() P_led = D_ledOff
 #define F_ledNeg() P_led = ~P_led
 // Function ------------------------------------
-void DisplayNumber(uint8_t number);
-void DisplayTenNumber(uint8_t number);
-void DisplayBitNumber(uint8_t number);
-void ClearNumber(uint8_t number);
+void SMG_Select(uint8_t position, uint8_t number);
+void SMG_Display(uint8_t number);
+void Set_Pin_Sta();
 #endif
