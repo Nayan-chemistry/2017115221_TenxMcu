429f71f5f489cc871c5d7d24a4beb300494492c1
Nayan-chemistry <1210148168@qq.com>
2020-01-05 19:31:22 +0800

    增加key4按键

--- prj5_hotkettle/code(68Bsop32)/keys.c
+++ prj5_hotkettle/code(68Bsop32)/keys.c
@@ -1,23 +1,26 @@
 #define __keys_c
 #include "includeAll.h"
 //=============================================================================
+//=============================================================================
 //按键信息获取
 void GetKeys() {
 	static uint8_t tempKeyValue = D_keyNull;
+	static uint8_t tempKeySelect = 0;
 	
-//	delayMs(10); //去抖，提高按键的稳定性
-	if(P_key4 == 0){
+	//按键按下
+	if(P_key4==0 || P_key3==0){
 		tempKeyValue = D_keyValue1;
-	}	else{
+		
+		//检测按下选择的按键
+		if(P_key4==0)
+			tempKeySelect = 4;
+		else if(P_key3==0)
+			tempKeySelect = 3;		
+	}	
+	else{
 		keyValue = tempKeyValue;
+		keySelect = tempKeySelect;
 		tempKeyValue = D_keyNull;
+		tempKeySelect = 0;
 	}
-	/*
-	if(P_key3 == 0){
-		tempKeyValue = D_keyValue1;
-	}	else{
-		keyValue3 = tempKeyValue;
-		tempKeyValue = D_keyNull;
-	}
-	*/
 }
